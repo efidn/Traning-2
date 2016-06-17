e182462409b2e22f2b777de2866808b2f7b99a7b
Effi Diner Nahcmani <Effi Diner Nahcmani@EFFI>
2016-06-17 11:31:22 +0200

    first change

---
+++ hellowWorld/hellowWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._1_Hello_World_Program.hellowWorld
+{
+    public class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, C#!");
+            Console.WriteLine();
+
+            string[] prints = { "hello world", "Effi Diner Nachmani", "Haifa, Shimshom 20/2" };
+
+            Console.WriteLine(" My first Strings and Console APP: ");
+            Console.WriteLine("***********************************");
+
+            foreach (string print in prints)
+            {
+                Console.WriteLine(print);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
